37868046270925acfbbcb5b8b3c761dc9b217163
michaelslain <misha.slain@icloud.com>
2020-08-10 11:22:04 -0700

    Beta

---
+++ desktop/public/electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const electronIsDev = require('electron-is-dev')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    })
+
+    if (electronIsDev) win.loadURL('http://localhost:3000')
+    else win.loadFile('../build/index.html')
+
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
---
+++ desktop/src/components/sound.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+import './sound.scss'
+
+export default function Sound({
+    sound,
+    staticImage,
+    dynamicImage,
+    name,
+    isPlaying,
+    stopReloadCounter,
+}) {
+    const [isDynamic, setIsDynamic] = useState(false)
+    const [volume, setVolume] = useState(0)
+    const [audio, setAudio] = useState(new Audio(sound))
+
+    useEffect(() => {
+        handleAudioInit()
+    }, [])
+
+    useEffect(() => {
+        handlePlayAudio()
+    }, [isDynamic])
+
+    useEffect(() => {
+        handleDynamicState()
+        handleAudioVolume()
+    }, [volume, isPlaying])
+
+    useEffect(() => {
+        if (stopReloadCounter > 0) setVolume(0)
+    }, [stopReloadCounter])
+
+    const handleAudioInit = () => (audio.loop = true)
+    const handleDynamicState = () => {
+        if (volume < 1) {
+            setIsDynamic(false)
+            return
+        }
+        if (!isPlaying) {
+            setIsDynamic(false)
+            return
+        }
+        setIsDynamic(true)
+    }
+    const handlePlayAudio = () => {
+        if (isDynamic) audio.play()
+        else audio.pause()
+    }
+    const handleAudioVolume = () => (audio.volume = volume / 100)
+    const handleChangeVolume = e => setVolume(e.target.value)
+
+    const image = isDynamic ? dynamicImage : staticImage
+
+    return (
+        <div className="sound">
+            <img src={image} alt={name} className="sound-image" />
+            <h1 className="sound-title">{name}</h1>
+            <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                className="sound-slider"
+                onChange={handleChangeVolume}
+            />
+        </div>
+    )
+}
